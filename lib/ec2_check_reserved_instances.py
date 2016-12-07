fd940c58c48c9c0b188dd3f723f2a37a2c881b82
Oluwaseun Obajobi <oba@obajobi.com>
2016-12-07 14:43:42 +0400

    separate linux and windows instances

--- lib/ec2_check_reserved_instances.py
+++ lib/ec2_check_reserved_instances.py
@@ -33,17 +33,18 @@ def main():
 			else:
 				az = instance['Placement']['AvailabilityZone']
 				instance_type = instance['InstanceType']
-				running_instances[ (instance_type, az ) ] = running_instances.get( (instance_type, az ) , 0 ) + 1
-
-				for tag in instance['Tags']:
+				platform = instance.get('Platform')
+				if not platform:
+					platform = u'linux'
+				running_instances[ (instance_type, az, platform ) ] = running_instances.get( (instance_type, az, platform ) , 0 ) + 1
+				for tag in instance.get('Tags'):
 					if "Name" == tag['Key'] and len(tag['Value']) > 0:
 						tag_name = tag['Value']
 						break
 					else:
 						continue
 				tag_name = tag_name if tag_name else instance['InstanceId']
-				instance_ids[ (instance_type, az ) ].append(tag_name)
-
+				instance_ids[ (instance_type, az, platform ) ].append(tag_name)
 
 	logger.debug("Running instances: %s"% pformat(running_instances))
 
@@ -55,7 +56,13 @@ def main():
 		else:
 			az = reserved_instance['AvailabilityZone']
 			instance_type = reserved_instance['InstanceType']
-			reserved_instances[( instance_type, az) ] = reserved_instances.get ( (instance_type, az ), 0 )  + reserved_instance['InstanceCount']
+			logger.debug("Reserved instance: %s"% (reserved_instance))
+			description = reserved_instance['ProductDescription']
+			if "Windows" in description:
+				platform = u'windows'
+			else:
+				platform = u'linux'
+			reserved_instances[( instance_type, az, platform) ] = reserved_instances.get ( (instance_type, az, platform ), 0 )  + reserved_instance['InstanceCount']
 
 	logger.debug("Reserved instances: %s"% pformat(reserved_instances))
 
@@ -75,7 +82,7 @@ def main():
 		print "Congratulations, you have no unused reservations"
 	else:
 		for unused_reservation in unused_reservations:
-			print "UNUSED RESERVATION!\t(%s)\t%s\t%s" % ( unused_reservations[ unused_reservation ], unused_reservation[0], unused_reservation[1] )
+			print "UNUSED RESERVATION!\t(%s)\t%s\t%s\t%s" % ( unused_reservations[ unused_reservation ], unused_reservation[0],unused_reservation[2], unused_reservation[1] )
 
 	print ""
 
@@ -87,7 +94,12 @@ def main():
 			ids=""
 			if args.names:
 				ids = ', '.join(sorted(instance_ids[unreserved_instance]))
-			print "Instance not reserved:\t(%s)\t%s\t%s\t%s" % ( unreserved_instances[ unreserved_instance ], unreserved_instance[0], unreserved_instance[1], ids )
+			print "Instance not reserved:\t(%s)\t%s\t%s\t%s\t%s" % ( unreserved_instances[ unreserved_instance ], unreserved_instance[0], unreserved_instance[2], unreserved_instance[1], ids )
+
+	if unused_reservations.values():
+		qty_unused_reservations = reduce( lambda x, y: x+y, unused_reservations.values() )
+	else:
+		qty_unused_reservations = 0
 
 	if running_instances.values():
 		qty_running_instances = reduce( lambda x, y: x+y, running_instances.values() )
@@ -99,4 +111,4 @@ def main():
 	else:
 		qty_reserved_instances = 0
 
-	print "\n(%s) running on-demand instances\n(%s) reservations" % ( qty_running_instances, qty_reserved_instances )
+	print "\n(%s) running on-demand instances\n(%s) reservations\n(%s) unused reservations" % ( qty_running_instances, qty_reserved_instances,qty_unused_reservations )
